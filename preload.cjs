63035767da956209aa114d8676b15e00e7d0a90e
7venik-bit <7venik@gmail.com>
2026-07-25 22:35:01 +0900

    Add preload.cjs

---
+++ preload.cjs
@@ -0,0 +1,22 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("aroundG", {
+  snapshot: () => ipcRenderer.invoke("store:snapshot"),
+  upsert: (collection, item) => ipcRenderer.invoke("store:upsert", collection, item),
+  remove: (collection, id) => ipcRenderer.invoke("store:remove", collection, id),
+  saveConfig: (config) => ipcRenderer.invoke("config:save", config),
+  getConfig: () => ipcRenderer.invoke("config:get"),
+  queryPoizon: (input) => ipcRenderer.invoke("poizon:query", input),
+  importExcel: () => ipcRenderer.invoke("excel:import"),
+  exportExcel: () => ipcRenderer.invoke("excel:export"),
+  openExternal: (url) => ipcRenderer.invoke("external:open", url),
+  collectorCheck: (input) => ipcRenderer.invoke("collector:check", input)
+  ,
+  checkForUpdates: () => ipcRenderer.invoke("update:check"),
+  installUpdate: () => ipcRenderer.invoke("update:install"),
+  onUpdateStatus: (callback) => {
+    const handler = (_event, payload) => callback(payload);
+    ipcRenderer.on("update:status", handler);
+    return () => ipcRenderer.removeListener("update:status", handler);
+  }
+});
