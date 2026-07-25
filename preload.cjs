c08967fd30c1d702c5e715601af3c112817c184c
7venik-bit <7venik@gmail.com>
2026-07-26 08:35:13 +0900

    Keep product explorer open after paste

--- preload.cjs
+++ preload.cjs
@@ -3,10 +3,10 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("aroundG", {
   snapshot: () => ipcRenderer.invoke("store:snapshot"),
   upsert: (collection, item) => ipcRenderer.invoke("store:upsert", collection, item),
+  bulkUpsert: (collection, items) => ipcRenderer.invoke("store:bulk-upsert", collection, items),
   remove: (collection, id) => ipcRenderer.invoke("store:remove", collection, id),
   saveConfig: (config) => ipcRenderer.invoke("config:save", config),
   getConfig: () => ipcRenderer.invoke("config:get"),
-  queryPoizon: (input) => ipcRenderer.invoke("poizon:query", input),
   explorerMeta: () => ipcRenderer.invoke("explorer:meta"),
   parsePopular: (input) => ipcRenderer.invoke("explorer:popular", input),
   resolvePopular: (input) => ipcRenderer.invoke("explorer:popular-resolve", input),
@@ -16,7 +16,6 @@ contextBridge.exposeInMainWorld("aroundG", {
     return () => ipcRenderer.removeListener("explorer:popular-progress", handler);
   },
   queryExplorer: (input) => ipcRenderer.invoke("explorer:query", input),
-  queryDomestic: (input) => ipcRenderer.invoke("domestic:query", input),
   importExcel: () => ipcRenderer.invoke("excel:import"),
   exportExcel: () => ipcRenderer.invoke("excel:export"),
   openExternal: (url) => ipcRenderer.invoke("external:open", url),
