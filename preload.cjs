80d0d069e9ded8c07a67f7a9cd99c37b466bc702
7venik-bit <7venik@gmail.com>
2026-08-09 17:58:05 +0900

    refactor: remove brand search preload bridge

--- preload.cjs
+++ preload.cjs
@@ -23,11 +23,6 @@ contextBridge.exposeInMainWorld("aroundG", {
     return () => ipcRenderer.removeListener("explorer:brand-progress", handler);
   },
   openSellerCenter: () => ipcRenderer.invoke("seller:open"),
-  openSellerProductSearch: () => ipcRenderer.invoke("seller:open-product-search"),
-  automateSellerBrandExport: (input) => ipcRenderer.invoke("seller:brand-export", input),
-  abortSellerBrandExportAttempt: () => ipcRenderer.invoke("seller:abort-brand-export-attempt"),
-  startSellerBrandExportMonitor: () => ipcRenderer.invoke("seller:start-brand-export-monitor"),
-  listPendingBrandExportJobs: () => ipcRenderer.invoke("brand-export:pending-jobs"),
   openDownloadedBrandFile: (path, brand) => ipcRenderer.invoke("brand-export:open-file", { path, brand }),
   openOriginalExcelFile: (path) => ipcRenderer.invoke("brand-export:open-original", { path }),
   previewExcelFile: (path, offset = 0, limit = 100, filters = {}) => ipcRenderer.invoke("excel:preview", { path, offset, limit, filters }),
